eb95ca165da1ed52104e4406bb684ff3f3cae8c9
David Ryda <rydadave@gmail.com>
2017-11-02 19:07:41 -0400

    first commit

---
+++ Lab9/Circle.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab9
+{
+    class Circle
+    {
+        public const double pi = Math.PI;
+        private double radius;
+        private double cir;
+        private double area;
+
+        public Circle(double radius)
+        {
+            this.radius = radius;
+        }
+
+        public double GetCircumference()
+        {
+            cir = 2 * pi * radius;
+            return cir;
+        }
+    
+        public string GetFormattedCircumference()
+        {
+            return FormatNumber(cir);
+        }
+
+        public double GetArea()
+        {
+            area =  pi * radius * radius;
+            return area;
+        }
+
+        public string GetFormattedArea()
+        {
+            return FormatNumber(area);
+        }
+
+        private string FormatNumber(double x)
+        {
+            string value = string.Format("{0:0.00}", x);
+            return value;
+        }
+    }
+}
---
+++ Lab9/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab9
+{
+    class Program
+    {
+        public const double pi = Math.PI;
+        public static int counter = 1;
+
+        static void Main(string[] args)
+        {
+            bool loop = true;
+            Console.WriteLine("Welcome to the Circle Tester");
+            while (loop)
+            {
+                Console.Write("Enter radius: ");
+                string input = Console.ReadLine().Trim();
+                double.TryParse(input, out double radius);
+                Circle createdCircle = new Circle(radius);
+
+                createdCircle.GetCircumference();
+                createdCircle.GetArea();
+
+                Console.WriteLine("Circumference: " + createdCircle.GetFormattedCircumference());
+                Console.WriteLine("Area: " + createdCircle.GetFormattedArea());
+
+                Console.WriteLine("Continue? (Y or N)");
+                char answer = Console.ReadLine().ToLower().Trim()[0];
+                if (answer == 'y')
+                {
+                    counter++;
+                    loop = true;
+                }
+                else
+                {
+                    Console.WriteLine("You made {0} circle(s)!", counter);
+                    loop = false;
+                }
+            }
+            //Write the total number of built circles
+
+            Console.ReadKey();
+
+            /*bool loop = true;
+            Console.WriteLine("Welcome to the Circle Tester");
+            while (loop)
+            {
+                Console.Write("Enter radius: ");
+                string input = Console.ReadLine().Trim();
+                double radius = 0.0;
+                double.TryParse(input, out radius);
+
+                double circumference = 2 * pi * radius;
+                double area = pi * radius * radius;
+
+                double roundedCircumference = Math.Round(circumference, 2);
+                double roundedArea = Math.Round(area, 2);
+
+                Console.WriteLine("Circumference: " + roundedCircumference);
+                Console.WriteLine("Area: " + roundedArea);
+                Console.WriteLine("Continue? (y or N)");
+                char answer = Console.ReadLine().ToLower().Trim()[0];
+                if (answer == 'y')
+                {
+                    loop = true;
+                }
+                else
+                {
+                    Environment.Exit(0);
+                }
+            }*/
+        }
+    }
+}
